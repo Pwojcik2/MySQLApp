0baa75731a9b6ecc182602d520e9974bba2fca05
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-03-03 14:46:12 -0700

    dbservice

--- server/dbService.js
+++ server/dbService.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv')
+let instance = null;
 dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USERNAME,
+    user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     port: process.env.PORT
@@ -16,3 +17,49 @@ connection.connect((err) => {
     }
     console.log('db' + connection.state)
 })
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService()
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names;"
+
+                connection.query(query, (err, results) => {
+                    if(err) reject(new Error(err.message))
+                    resolve(results)
+                })
+            })
+            
+            return response;
+            console.log(response)
+
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    async insertNewName(name) {
+        try {
+            const dateCreated = new Date();
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO names (name, date_created) VALUES (?, ?);";
+
+                connection.query(query, [name. dateCreated] ,(err, result) => {
+                    if(err) reject(new Error(err.message))
+                    resolve(result.insertId)
+                })
+            })
+            console.log(insertId)
+            // return response;
+
+        } catch(err) {
+            console.log(err)
+        }
+    }
+}
+
+module.exports = DbService;
