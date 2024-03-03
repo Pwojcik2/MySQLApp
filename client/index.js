3ec2464a88623a08c74265659996f4217b9ddd5b
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-03-03 12:18:08 -0700

    added empty data array

---
+++ client/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadTable([]);
+});
+
+function loadTable(data) {
+    const table = document.querySelector('table tbody')
+    let tableData = ''
+
+    if(data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    }
+}
